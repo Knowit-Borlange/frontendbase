0d8a0c418cd5d8e9b4bfdc89f8ef6a8f54417de8
Daniel Gustafsson <daniel.gustafsson@knowit.se>
2021-07-09 14:20:41 +0200

    Refactoring and removing some stuff

--- app/framework/app.js
+++ app/framework/app.js
@@ -1,28 +1,6 @@
 'use strict';
+window.Popper = require('@popperjs/core');
 
-
-import $ from 'jquery';
-window.$ = window.jQuery = require('jquery');
-window.Popper = require('popper.js');
 require('bootstrap');
 
-
 require('what-input');
-
-
-// ----- Breakpoint checker
-// -----
-import checkBreakpoint from './js/check-breakpoint/checkBreakpoint';
-checkBreakpoint.init();
-// Use as below in component
-// import checkBreakpoint from '../../framework/js/check-breakpoint/checkBreakpoint';
-// if (checkBreakpoint.check(['xs', 'sm', 'md'])) {}
-
-
-// ----- Navigations
-// -----
-import "./components/c-nav--side/c-side-nav-over.js";
-
-
-import shortcutsBreakpointMove from './components/c-nav--shortcuts/shortcutsBreakpointMove';
-shortcutsBreakpointMove.init();
